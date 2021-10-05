4b6b98b2e00b99f2138a9319f21b55866685398d
Sagar Chauhan <chauhansingh1234@gmail.com>
2021-10-06 03:18:54 +0530

    move.js file modified

--- camelMoveOnChessBoard/move.js
+++ camelMoveOnChessBoard/move.js
@@ -1,96 +1,101 @@
-
-var S="",S_length=0;
-var e_color;
+//globle varibles
+var S='',SLength=0;
+var eColor;
 var G_xp,G_yp;
-function position(X,Y)
-{
-	G_xp=X;G_yp=Y
-	var E_id =""+X+","+Y;
-	e_color = document.getElementById(E_id).style.backgroundColor;
+var N=7;
 
-	var x=X,y=Y,N=7,s="";
+//function to get positions where camel can move in these directions: 'S-E','N-E', 'S-W', 'N-W'
+function position(xDir,yDir) {
+	var eId;
+	G_xp = xDir;
+	G_yp = yDir;
+	eId = xDir+','+yDir;
+	eColor = document.getElementById(eId).style.backgroundColor;
 
+	//positions for S-E direction
+	let col=xDir,row=yDir;
 	document.getElementById('h1').innerHTML="S-E positions";
-	   while(x<N && y>0)
-	   {
-		  s=s.concat((x+1)+","+(y-1)+" ");
-	      color(x,y);
-	      x++;
-	      y--;
-	      S_length++;
+	   while(col<N && row>0) {
+		   var sE = '';
+		   let x = col, y = row;
+		   sE = sE.concat((x+1)+','+(y-1));
+		   color(x,y);
+		   x++;
+		   y--;
+		   SLength++;
 	   }
-		color(x,y);
-		S=S.concat(s);
-		document.getElementById('p1').innerHTML=s;
-		s="";
+	   S=S.concat(sE);
+	   document.getElementById('p1').innerHTML = sE;
+	   sE = '';
 
-	 x=X;y=Y;
+	//positions for N-E direction
 	 document.getElementById('h2').innerHTML="N-E positions";
-	   while(x>0 && y>0)
-	   {
-	      s=s.concat((x-1)+","+(y-1)+" ");
-	      color(x,y);
-	      x--;
-	      y--;
-	      S_length++;
+	   while(col>0 && row>0) {
+		   var nE = '';
+		   let x = col, y = row;
+		   nE = nE.concat((x-1)+","+(y-1)+" ");
+		   color(x,y);
+		   x--;
+		   y--;
+		   SLength++;
 	   }
-		color(x,y);
-		S=S.concat(s);
-		document.getElementById('p2').innerHTML=s;
-		s="";
+	   S=S.concat(nE);
+	   document.getElementById('p2').innerHTML= nE;
+	   nE = '';
 
-	 x=X;y=Y;
+	//positions for S-W direction
 	 document.getElementById('h3').innerHTML="S-W positions";
-	   while(x<N && y<N)
-	   {
-	      s=s.concat((x+1)+","+(y+1)+" ");
-	      color(x,y);
-	      x++;
-	      y++;
-	      S_length++;
+	   while(col<N && row<N) {
+		   var sW = '';
+		   let x = col, y = row;
+		   sW = sW.concat((x+1)+','+(y+1));
+		   color(x,y);
+		   x++;
+		   y++;
+		   SLength++;
 	   }
-		color(x,y);
-		S=S.concat(s);
-		document.getElementById('p3').innerHTML=s;
-		s="";
-
-	   x=X;y=Y;
+		S=S.concat(sW);
+		document.getElementById('p3').innerHTML = sW;
+		sW = '';
+	
+	//positions for N-W direction
 	   document.getElementById('h4').innerHTML="N-W positions";
-	   while(x>0 && y<N)
-	   {
-	      s=s.concat((x-1)+","+(y+1)+" ");
-	      color(x,y);
-	      x--;
-	      y++;
-	      S_length++;
-   	   }
-		color(x,y);
-		S=S.concat(s);
-   	    document.getElementById('p4').innerHTML=s;
-   	    s="";
+	   while(x>0 && y<N) {
+		   var nW = '';
+		   let x = col; y = row;
+		   s=s.concat((x-1)+','+(y+1));
+		   color(x,y);
+		   x--;
+		   y++;
+		   SLength++;
+	   }
+	   S=S.concat(nW);
+	   document.getElementById('p4').innerHTML = nW;
+	   nW = '';
+	   color(col,row);
 
 }
-function color(X,Y)
-{
-	var s="";
-	s = ""+X+","+Y;
+
+//function to color possible positons where camel can move
+function color(col,row) {
+	let s = col+','+row;
 	document.getElementById(s).style.backgroundColor="lightgreen";
 }
-function reset_color(z)
-{
-	let s = ""+G_xp+","+G_yp;
+
+//function to reset color 
+function resetColor(z) {
+	let s = G_xp+','+G_yp;
 	G_xp=0;
 	G_xy=0;
 	document.getElementById(s).style.backgroundColor=e_color;
 	//alert(S);
-	var arr =z.split(" ");
+	let arr =z.split(' ');
 	//alert(arr);
 	//alert(e_color);
-	for(let i=0; i<S_length; i++)
-	{
+	for(let i=0; i<SLength; i++) {
 		document.getElementById(arr[i]).style.backgroundColor=e_color;
 	}
-	S_length=0;
-	S="";
+	SLength = 0;
+	S = '';
 	//alert(arr);
 }
